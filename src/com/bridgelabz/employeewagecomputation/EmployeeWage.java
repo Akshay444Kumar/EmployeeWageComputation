e7d3253e7ff7425cd9d92961ac3f6b1e59efb2fc
Akshay444Kumar <aryakshay444@gmail.com>
2023-10-09 12:19:02 +0530

    Added: Program to check if Employee is present

---
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        int check = (int) Math.floor(Math.random() * 10 % 2);
+        if (check == 1) {
+            System.out.println("Employee is Present.");
+        } else {
+            System.out.println("Employee is Absent.");
+        }
+    }
+}
