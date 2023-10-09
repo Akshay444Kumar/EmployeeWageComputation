e11fbe21a08e8deab8c4a3b8c181b513b076101e
Akshay444Kumar <aryakshay444@gmail.com>
2023-10-09 13:19:33 +0530

    Added: Employee Wage Calculation using Switch case

--- src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWage.java
@@ -8,14 +8,27 @@ public class EmployeeWage {
         int PART_TIME_HOURS = 4;
         int dailyWage = 0;
         int check = (int) Math.floor(Math.random() * 10 % 3);
-        if (check == 1) {
-            System.out.println("Employee is Present.");
-            dailyWage = FULL_DAY_HOURS * WAGE_PER_HOUR;
-        } else if (check == 2) {
-            System.out.println("Employee is Part-time.");
-            dailyWage = PART_TIME_HOURS * WAGE_PER_HOUR;
-        } else {
-            System.out.println("Employee is Absent.");
+//        if (check == 1) {
+//            System.out.println("Employee is Present.");
+//            dailyWage = FULL_DAY_HOURS * WAGE_PER_HOUR;
+//        } else if (check == 2) {
+//            System.out.println("Employee is Part-time.");
+//            dailyWage = PART_TIME_HOURS * WAGE_PER_HOUR;
+//        } else {
+//            System.out.println("Employee is Absent.");
+//        }
+        // Using Switch Case
+        switch (check) {
+            case 1:
+                System.out.println("Employee is Full-time.");
+                dailyWage = FULL_DAY_HOURS * WAGE_PER_HOUR;
+                break;
+            case 2:
+                System.out.println("Employee is Part-time.");
+                dailyWage = PART_TIME_HOURS * WAGE_PER_HOUR;
+                break;
+            default:
+                System.out.println("Employee is Absent.");
         }
         System.out.println("The total Wage of the Employee is: " +dailyWage);
     }
